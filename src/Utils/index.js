0b5ef7c1113e2c6f7d9b322e95aad790b27c90c0
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-07 00:56:54 -0300

    feat: add reply with meme

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,28 +1,47 @@
+import 'dotenv/config';
 import lansaMeme from '../Image';
 
-export function TweetMeme(client) {
-  client.post('media/upload', {media: lansaMeme}, function(error, media, response) {
-    if (!error) {
-      console.log(media);
-      var status = {
-        status: 'Lansou',
-        media_ids: media.media_id_string 
+export function TweetMeme(client, user, in_reply_to_status_id) {
+  return new Promise((resolve, reject) => {
+    client.post('media/upload', { media: lansaMeme }, function(error, media) {
+      if (!error) {
+        const status = {
+          status: `@${user}`,
+          media_ids: media.media_id_string,
+          auto_populate_reply_metadata: true,
+          in_reply_to_status_id,
+        };
+
+        client.post('statuses/update', status, function(err, tweet) {
+          if (!err) {
+            resolve(tweet);
+          } else {
+            reject(err);
+          }
+        });
+      } else {
+        reject(error);
       }
-  
-      client.post('statuses/update', status, function(error, tweet, response) {
-        if (!error) {
-          console.log(tweet);
-        }
-      });
-    }
+    });
   });
 }
 
-
-export function Replies(client) {
-  client.get('statuses/mentions_timeline', (error, replies, response) => {
-    if(!error){
-      return replies;
-    }
+export function Mentions(client) {
+  return new Promise((resolve, reject) => {
+    client.get('statuses/mentions_timeline', (error, mentions) => {
+      if (!error) {
+        resolve(mentions);
+      } else {
+        reject(error);
+      }
+    });
   });
+}
+
+export function checkReply(reply, lastReply) {
+  if (String(reply.id) === lastReply) {
+    return false;
+  }
+  console.log('Must Reply!');
+  return true;
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import Twitter from 'twitter';
 
 import config from './AuthKeys';
-import { TweetMeme, Replies } from './Utils';
+import { TweetMeme, Mentions, checkReply } from './Utils';
 
 const client = new Twitter(config);
 
-TweetMeme(client)
+let lastReply = '';
 
+setInterval(() => {
+  console.log('Checking mentions again...');
+
+  Mentions(client)
+    .then(mentions => {
+      if (mentions.length !== 0) {
+        const mustReply = checkReply(mentions[0], lastReply);
+        if (mustReply) {
+          console.log('\n###');
+          console.log('Replying');
+          console.log('###');
+          lastReply = String(mentions[0].id);
+          TweetMeme(client, mentions[0].user.screen_name, mentions[0].id_str);
+        }
+      }
+      console.log('No new mentions...');
+    })
+    .catch(err => {
+      throw err;
+    });
+}, 5000);
